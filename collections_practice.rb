90fe31b7da8f7550a32bbb49a6f9b8e25182a168
monamohamed <mona.m.mohamed@gmail.com>
2020-12-12 21:38:49 -0500

    commit tree 8172cba0084909a5951bdb1693d37e5ebb830884

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc (integers)
+integers.sort! 
+end
 
+
+def sort_array_desc(integers)
+integers.sort! {|a,b| b <=> a}
+end
+
+
+def sort_array_char_count(array)
+array = array.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+array[1], array [2] = array [2], array[1]
+return array
+end
+
+def reverse_array(array)
+array.reverse!
+return array
+end
+
+def kesha_maker(strings)
+$_array =[] #like they asked 
+strings.each do |x|
+    i = x.gsub(x[2], "$")
+    $_array.push(i)
+end
+return $_array
+end
+
+def find_a(array)
+array.select{|m| m.start_with?("a")}
+end
+    
+def sum_array(array)
+array.inject(:+)
+end
+
+def add_s(array)
+array.each_with_index do |element, index|
+element[element.length] = "s" unless index == 1
+end
+end
+    
